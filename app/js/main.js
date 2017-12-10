7a949b19700d32772598ed705339dc6893856c3e
Alex <frozerth@gmail.com>
2017-12-10 14:35:59 +0200

    adaptive without menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,35 @@
-$( document ).ready(function() {
+	$( document ).ready(function() {
 	$("#menu li a").click(function(event) {
 			event.preventDefault();
 			var id  = $(this).attr('href'),
 			top = $(id).offset().top;
 			$('body,html').animate({scrollTop: top}, 1500);
+	});
+
+	if ($('.back-to-top').length) {
+    var scrollTrigger = 100,
+        backToTop = function () {
+          var scrollTop = $(window).scrollTop();
+          if (scrollTop > scrollTrigger) {
+              $('.back-to-top').addClass('show');
+          } else {
+              $('.back-to-top').removeClass('show');
+          }
+        };
+    backToTop();
+    $(window).on('scroll', function () {
+      backToTop();
+    });
+    $('.back-to-top').on('click', function (e) {
+      e.preventDefault();
+      $('html,body').animate({
+      	scrollTop: 0
+      }, 700);
+    });
+	};
+
+	$('.sandwich-menu').click(function(){
+		$(this).siblings('ul').show(function(){$(this).animate(),500});
 	});
 
 	$('.slider').slick({
